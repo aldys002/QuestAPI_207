a85314bbe7cc73316caeb6f481ca1b6688a3f9a9
aldys002 <igidiaaldys@gmail.com>
2025-12-18 14:11:06 +0700

    menambahkan HomeViewModel dan StatusUiSiswa untuk menampilkan data siswa

---
+++ app/src/main/java/com/example/restapi/view/viewmodel/HomeViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.restapi.view.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.restapi.modeldata.DataSiswa
+import com.example.restapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed interface StatusUiSiswa{
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa):
+ViewModel(){
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }catch (e: IOException){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
