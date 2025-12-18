80e9e72aadb753310add0f1b61b342392914395b
aldys002 <igidiaaldys@gmail.com>
2025-12-18 18:53:17 +0700

    menambah destinasinavigasi di line 5

--- app/src/main/java/com/example/restapi/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/restapi/view/route/DestinasiEntry.kt
@@ -2,7 +2,7 @@ package com.example.restapi.view.route
 
 import com.example.restapi.R
 
-object DestinasiEntry {
+object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
