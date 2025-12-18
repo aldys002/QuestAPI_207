e754079c2754921080fa9cd183110bfba7626ddc
aldys002 <igidiaaldys@gmail.com>
2025-12-18 18:53:52 +0700

    mengganti object jadi interface

--- app/src/main/java/com/example/restapi/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/restapi/view/route/DestinasiHome.kt
@@ -1,7 +1,7 @@
 package com.example.restapi.view.route
 
 import com.example.restapi.R
-object DestinasiHome {
+object DestinasiHome : DestinasiNavigasi{
     override val route = "home"
     override val titleRes = R.string.app_name
 }
