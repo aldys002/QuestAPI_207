ccb07ea3d946d7fa9013e65e41ec4809d1107cf0
aldys002 <igidiaaldys@gmail.com>
2025-12-18 18:54:18 +0700

    menambah fungsi validasiinput

--- app/src/main/java/com/example/restapi/view/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/restapi/view/viewmodel/EntryViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.restapi.view.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.restapi.modeldata.DetailSiswa
 import com.example.restapi.modeldata.UIStateSiswa
+import com.example.restapi.modeldata.toDataSiswa
 import com.example.restapi.repositori.RepositoryDataSiswa
 import retrofit2.Response
 
@@ -13,6 +16,12 @@ class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa):
             private set
 
     /*Fungsi untuk memvalidasi input*/
+    private fun validasiInput(uiState : DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
     fun updateUiState(detailSiswa: DetailSiswa){
         uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
