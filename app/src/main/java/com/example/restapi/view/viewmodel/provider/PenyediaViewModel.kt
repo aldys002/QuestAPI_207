35859b83c2e6bbe389176e81d353493fc50cc365
aldys002 <igidiaaldys@gmail.com>
2025-12-25 11:30:03 +0700

    nambah editviewmodel dan DetailViewModel.kt

--- app/src/main/java/com/example/restapi/view/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/restapi/view/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.restapi.view.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.restapi.repositori.AplikasiDataSiswa
+import com.example.restapi.view.viewmodel.DetailViewModel
+import com.example.restapi.view.viewmodel.EditViewModel
 import com.example.restapi.view.viewmodel.EntryViewModel
 import com.example.restapi.view.viewmodel.HomeViewModel
 
@@ -15,5 +18,17 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
     }
 }
